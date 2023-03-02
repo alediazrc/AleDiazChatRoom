dce9ab8e35a7ced652fded956069f4340c52297f
adiaz <adiaz@venon.solutions>
2023-03-02 14:48:28 -0300

    Basic Chat app working

---
+++ AleDiazChatRoom/Services/IChatHubService.cs
@@ -0,0 +1,10 @@
+﻿namespace AleDiazChatRoom.Services
+{
+    public interface IChatHubService
+    {
+        public Task Broadcast(string username, string message);
+        public Task OnConnectedAsync();
+        public Task OnDisconnectedAsync(Exception e);
+
+    }
+}
