17d262d91dbcb7cc2a39ce7413c7616a56db4110
adiaz <adiaz@venon.solutions>
2023-03-02 20:28:40 -0300

    Add Api project

---
+++ ChatRoomMessages/Controllers/ChatMessagesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatRoomMessages.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatMessagesController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStoredMessages()
+        {
+            return "Im a result";
+        }
+    }
+}
